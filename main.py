5e8f024d2c2d0c3fc23814819021f31c6b95d255
zxxhz <62083314+zxxhz@users.noreply.github.com>
2023-05-21 12:42:30 +0800

    Update main.py
    增加定时同步功能

--- main.py
+++ main.py
@@ -6,33 +6,48 @@ import json
 import requests
 from pymongo import MongoClient
 from datetime import datetime
+import schedule
+import time
 
+#读取配置文件
 with open("config.json", "r", encoding="utf-8") as config:
   config_data = json.load(config)
 
-#获取URL内容
-response = requests.get(config_data["url"])
-xml_content = response.content
 
-#将XML转换为JSON
-xml_dict = xmltodict.parse(xml_content)
-json_data = json.loads(json.dumps(xml_dict))
-#print(json_data)
+def sync_job():
+  #获取URL内容
+  response = requests.get(config_data["url"])
+  xml_content = response.content
 
-now = datetime.now()
-time_str = now.strftime("%Y%m%d")
+  #将XML转换为JSON
+  xml_dict = xmltodict.parse(xml_content)
+  json_data = json.loads(json.dumps(xml_dict))
 
-#建立与MongoDB的连接和选择数据库
-with MongoClient(config_data["DataBase"]) as client:
-  db = client["qnap"]
-  collection = db["officialxml" + time_str]
-  #增量存储到MongoDB
-  for item in json_data["plugins"]["item"]:
-    #假设数据的唯一标识符是"id"
-    document = collection.find_one({"name": item["name"]})
-    if document:
-      #如果文档已经存在，则更新
-      collection.update_one({"name": item["name"]}, {"$set": item})
-    else:
-      #如果文档不存在，则插入新文档
-      collection.insert_one(item)
+  #获取当前日期
+  now = datetime.now()
+  time_str = now.strftime("%Y%m%d%H")
+
+  #建立与MongoDB的连接和选择数据库
+  with MongoClient(config_data["DataBase"]) as client:
+    db = client["qnap"]
+    collection = db["officialxml" + time_str]
+    #增量存储到MongoDB
+    for item in json_data["plugins"]["item"]:
+      #假设数据的唯一标识符是"id"
+      document = collection.find_one({"name": item["name"]})
+      if document:
+        #如果文档已经存在，则更新
+        collection.update_one({"name": item["name"]}, {"$set": item})
+      else:
+        #如果文档不存在，则插入新文档
+        collection.insert_one(item)
+
+
+#读取配置中的时间生成任务
+for sync_time in config_data["sync_time"]:
+  schedule.every().day.at(sync_time).do(sync_job)
+
+while True:
+  # 运行所有可以运行的任务
+  schedule.run_pending()
+  time.sleep(60)
